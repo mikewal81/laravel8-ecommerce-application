c12e8c4d019ff26708e8bfa1e190f4ef6985f83e
Walusimbi Michael Ndugga <walusimbimichael@gmail.com>
2020-10-28 01:40:38 +0300

    initial commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
@@ -16,4 +16,6 @@ mix.copyDirectory('resources/frontend', 'public/frontend');
 mix.copyDirectory('resources/uploads', 'public/uploads');
 
 /* mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css'); */
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]); */
